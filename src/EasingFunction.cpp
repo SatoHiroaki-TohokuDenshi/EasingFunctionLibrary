c123e324c7d66ec8eedb3719ff7247da02df4ef5
央旺 佐藤 <220511@jc-21.jp>
2024-05-16 14:54:29 +0900

    2乗を利用したイージング関数の実装

--- src/EasingFunction.cpp
+++ src/EasingFunction.cpp
@@ -13,3 +13,16 @@ double EasingFuction::EasingOutSine(double x_) noexcept {
 double EasingFuction::EasingInOutSine(double x_) noexcept {
 	return -(std::cos(M_PI * x_) - 1.0) / 2.0;
 }
+
+double EasingFuction::EasingInQuad(double x_) noexcept {
+	return std::pow(x_, 2.0);
+}
+
+double EasingFuction::EasingOutQuad(double x_) noexcept {
+	return 1 - std::pow(1.0 - x_, 2.0);
+}
+
+double EasingFuction::EasingInOutQuad(double x_) noexcept {
+	if (x_ < 0.5)	return 2.0 * std::pow(x_, 2.0);
+	else			return 1 - std::pow(-2.0 * x_ + 2.0, 2.0) / 2;
+}
--- src/EasingFunction.hpp
+++ src/EasingFunction.hpp
@@ -14,4 +14,13 @@ namespace EasingFuction {
 	//sin関数を利用した、加速して減速するイージング
 	inline double EasingInOutSine(double x_) noexcept;
 
+	//2乗を利用した、徐々に加速するイージング
+	inline double EasingInQuad(double x_) noexcept;
+
+	//2乗を利用した、徐々に減速するイージング
+	inline double EasingOutQuad(double x_) noexcept;
+
+	//2乗を利用した、加速して減速するイージング
+	inline double EasingInOutQuad(double x_) noexcept;
+
 }
