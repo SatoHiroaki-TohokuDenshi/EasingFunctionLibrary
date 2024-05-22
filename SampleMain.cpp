a66c16f3968e7e3a890360cdd07e629fbc465c5d
央旺 佐藤 <220511@jc-21.jp>
2024-05-22 09:54:11 +0900

    Main内にサンプル用コードの実装

--- SampleMain.cpp
+++ SampleMain.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "src/EasingFunction.hpp"
 
 int main() {
-
+	for (double it = 0.0; it < 1.0; it += 0.01) {
+		double tmp = EasingFuction::EasingInOutBounce(it);
+		tmp *= 100.0;
+		for (int i = 0; i < std::round(tmp); i++) {
+			std::cout << "#";
+		}
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
--- src/EasingFunction.hpp
+++ src/EasingFunction.hpp
@@ -6,92 +6,92 @@
 ///
 namespace EasingFuction {
 	//sin関数を利用した、徐々に加速するイージング
-	inline double EasingInSine(double x_) noexcept;
+	double EasingInSine(double x_) noexcept;
 
 	//sin関数を利用した、徐々に減速するイージング
-	inline double EasingOutSine(double x_) noexcept;
+	double EasingOutSine(double x_) noexcept;
 
 	//sin関数を利用した、加速して減速するイージング
-	inline double EasingInOutSine(double x_) noexcept;
+	double EasingInOutSine(double x_) noexcept;
 
 	//2乗を利用した、徐々に加速するイージング
-	inline double EasingInQuad(double x_) noexcept;
+	double EasingInQuad(double x_) noexcept;
 
 	//2乗を利用した、徐々に減速するイージング
-	inline double EasingOutQuad(double x_) noexcept;
+	double EasingOutQuad(double x_) noexcept;
 
 	//2乗を利用した、加速して減速するイージング
-	inline double EasingInOutQuad(double x_) noexcept;
+	double EasingInOutQuad(double x_) noexcept;
 
 	//3乗を利用した、徐々に加速するイージング
-	inline double EasingInQubic(double x_) noexcept;
+	double EasingInQubic(double x_) noexcept;
 
 	//3乗を利用した、徐々に減速するイージング
-	inline double EasingOutQubic(double x_) noexcept;
+	double EasingOutQubic(double x_) noexcept;
 
 	//3乗を利用した、加速して減速するイージング
-	inline double EasingInOutQubic(double x_) noexcept;
+	double EasingInOutQubic(double x_) noexcept;
 
 	//4乗を利用した、徐々に加速するイージング
-	inline double EasingInQuart(double x_) noexcept;
+	double EasingInQuart(double x_) noexcept;
 
 	//4乗を利用した、徐々に減速するイージング
-	inline double EasingOutQuart(double x_) noexcept;
+	double EasingOutQuart(double x_) noexcept;
 
 	//4乗を利用した、加速して減速するイージング
-	inline double EasingInOutQuart(double x_) noexcept;
+	double EasingInOutQuart(double x_) noexcept;
 
 	//5乗を利用した、徐々に加速するイージング
-	inline double EasingInQuint(double x_) noexcept;
+	double EasingInQuint(double x_) noexcept;
 
 	//5乗を利用した、徐々に減速するイージング
-	inline double EasingOutQuint(double x_) noexcept;
+	double EasingOutQuint(double x_) noexcept;
 
 	//5乗を利用した、加速して減速するイージング
-	inline double EasingInOutQuint(double x_) noexcept;
+	double EasingInOutQuint(double x_) noexcept;
 
 	//指数を利用した、徐々に加速するイージング
-	inline double EasingInExpo(double x_) noexcept;
+	double EasingInExpo(double x_) noexcept;
 
 	//指数を利用した、徐々に減速するイージング
-	inline double EasingOutExpo(double x_) noexcept;
+	double EasingOutExpo(double x_) noexcept;
 
 	//指数を利用した、加速して減速するイージング
-	inline double EasingInOutExpo(double x_) noexcept;
+	double EasingInOutExpo(double x_) noexcept;
 
 	//円を利用した、徐々に加速するイージング
-	inline double EasingInCirc(double x_) noexcept;
+	double EasingInCirc(double x_) noexcept;
 
 	//円を利用した、徐々に減速するイージング
-	inline double EasingOutCirc(double x_) noexcept;
+	double EasingOutCirc(double x_) noexcept;
 
 	//円を利用した、加速して減速するイージング
-	inline double EasingInOutCirc(double x_) noexcept;
+	double EasingInOutCirc(double x_) noexcept;
 	
 	//最初に少し戻るイージング
-	inline double EasingInBack(double x_) noexcept;
+	double EasingInBack(double x_) noexcept;
 
 	//最後に行き過ぎてから戻るイージング
-	inline double EasingOutBack(double x_) noexcept;
+	double EasingOutBack(double x_) noexcept;
 
 	//少し戻って、行き過ぎてから戻るイージング
-	inline double EasingInOutBack(double x_) noexcept;
+	double EasingInOutBack(double x_) noexcept;
 
 	//弾性のある入りのイージング
-	inline double EasingInElastic(double x_) noexcept;
+	double EasingInElastic(double x_) noexcept;
 
 	//弾性のある終わりのイージング
-	inline double EasingOutElastic(double x_) noexcept;
+	double EasingOutElastic(double x_) noexcept;
 
 	//弾性のある入りと終わりのイージング
-	inline double EasingInOutElastic(double x_) noexcept;
+	double EasingInOutElastic(double x_) noexcept;
 
 	//弾む動きの入りののイージング
-	inline double EasingInBounce(double x_) noexcept;
+	double EasingInBounce(double x_) noexcept;
 
 	//弾む動きの終わりのイージング
-	inline double EasingOutBounce(double x_) noexcept;
+	double EasingOutBounce(double x_) noexcept;
 
 	//弾む動きの入りと終わりのイージング
-	inline double EasingInOutBounce(double x_) noexcept;
+	double EasingInOutBounce(double x_) noexcept;
 }
